34581b3954771f650005f582dae5e5fc80375240
LoGaNsF <logansf@gmail.com>
2021-12-27 15:57:07 +0100

    Day #23

---
+++ day23/index.test.js
@@ -0,0 +1,31 @@
+import canReconfigure from ".";
+
+describe("#23 - canReconfigure", () => {
+  const checkValidReconfigurations = [
+    { from: "BAL", to: "LIB" },
+    { from: "XBOX", to: "XOBX" },
+    { from: "xxxMMM", to: "aaaDDD" },
+    { from: "abcdefghijkl", to: "lkjihgfedcba" },
+];
+
+const checkInvalidReconfigurations = [
+    { from: "CON", to: "JUU" },
+    { from: "XBOX", to: "XXOB" },
+    { from: "MMM", to: "MID" },
+    { from: "AA", to: "MID" },
+    { from: "MID", to: "AA" },
+    { from: "xxxqqqeeefffggg", to: "xxxqqqeeefffgg" },
+  ];
+
+  test("should return that the reconfiguration is valid", () => {
+    checkValidReconfigurations.forEach((check) => {
+      expect(canReconfigure(check.from, check.to)).toBeTruthy();
+    });
+  });
+
+  test("should return that the reconfiguration is invalid", () => {
+    checkInvalidReconfigurations.forEach((check) => {
+      expect(canReconfigure(check.from, check.to)).toBeFalsy();
+    });
+  });
+});
